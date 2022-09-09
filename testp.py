a4d51e513d514c52ccc99f2137c1590afd5a76b6
AlHamzaMridha <112957413+AlHamzaMridha@users.noreply.github.com>
2022-09-09 02:41:16 -0700

    Add files via upload
    this is the main python file for products.

---
+++ testp.py
@@ -0,0 +1,11 @@
+list = {'apple':'250tk','orrange':'350tk','mango':'250tk','guava':'280tk','egg':'90tk per hali'}
+name = input('Type the name of product for price #BDT : ')
+print(list[name])
+
+if 'apple' in list:
+    print('available in shop')
+
+
+
+
+
